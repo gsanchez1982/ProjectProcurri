bebf048c088af3d992c233d5095e2a87d3529556
gsanchez1982 <gsemx@outlook.com>
2022-06-13 20:36:56 -0500

    Changing the App Controller again to test something else (previous test failed).

--- src/main/java/net/codejava/controller/AppController.java
+++ src/main/java/net/codejava/controller/AppController.java
@@ -39,7 +39,7 @@ public class AppController {
 
     @RequestMapping("/login")
     public String login(HttpSession session) {
-        session.setAttribute("mySessionAttribute", "Bienvenido");
+        session.setAttribute("mySessionAttribute", "Welcome");
 
         return "redirect:/";
     }
