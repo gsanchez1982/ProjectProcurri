a9c3fa6f002b178d023fdd049abfdc6ee9fc190f
gsanchez1982 <gsemx@outlook.com>
2022-06-28 21:26:50 -0500

    Reverse changes.

--- src/main/java/net/codejava/entity/Deals.java
+++ src/main/java/net/codejava/entity/Deals.java
@@ -19,7 +19,7 @@ public class Deals implements Serializable {
 	private String vInv; // Vendor Invoice
         private String OC; // Purchase order from Customer
         private String dealComments; // Comments related to the deal
-        @DateTimeFormat(pattern ="dd-MMM-yyyy")
+        @DateTimeFormat(pattern ="yyyy-MM-dd")
         private Date ocDate;
 
 	public Deals() {
